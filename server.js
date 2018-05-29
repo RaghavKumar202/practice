7c053a1e6b21c8f49e742ee25cddbd9b409f7531
RaghavKumar202 <raghavkumar202@gmail.com>
2018-05-29 13:07:54 +0530

    server

---
+++ server.js
@@ -0,0 +1,12 @@
+var express = require('express');
+var app = express();
+var Routes = express.Router();
+var port = process.env.PORT || 8080;
+
+app.get('/hello', function(request, response){
+	console.log("hello API get response");
+	response.send({message: 'hello API get response'});
+})
+app.listen(port);
+
+console.log('Magic happens at http://localhost:' + port);
